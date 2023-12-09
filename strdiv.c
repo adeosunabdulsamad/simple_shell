fca490546481bc0546dd512b793d54acb7a500e3
Adeosun Abdulsamad Ayomide <adeosunabulsamad25@gmail.com>
2023-12-09 11:00:47 +0100

    committed

--- strdiv.c
+++ strdiv.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+int main()
+{
+	char str[100];
+	const char s[2] =" ";
+	char* token;
+
+	printf("enter your text: ");
+	fgets(str, sizeof(str), stdin);
+        token = strtok(str, s);
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, s);
+	}
+	return 0;
+}
