a2e8d8f7807bba36129c4f4468c79e34ae7192d9
Adeosun Abdulsamad Ayomide <adeosunabulsamad25@gmail.com>
2023-12-13 10:39:18 +0100

    Updated getl.c with strtok function

--- getl.c
+++ getl.c
@@ -5,12 +5,19 @@ int main()
         char *buffer;
 	size_t bufsize = 0;
 	ssize_t characters;
+	char *token;
 
 	buffer = NULL;
 	while (1)
 	{
 		printf("$ ");
 		characters = getline(&buffer, &bufsize, stdin);
+		token = strtok(str, " ");
+		while (token != NULL)
+	    {
+			printf("%s\n", token);
+			token = strtok(NULL, s);
+	    }
 		if (characters == -1)
 		{
 			printf("The shell has closed\n");
